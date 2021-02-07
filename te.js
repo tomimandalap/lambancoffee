e3613247372a0e112c4a4c5ada39ca13f3f8a12f
tomimandalap <tommymandalaputra@gmail.com>
2021-02-07 12:05:00 +0700

    second commit

--- te.js
+++
@@ -1,8 +0,0 @@
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-}
-
-const data = getRandomInt(10, 1000)
-console.log(data)
